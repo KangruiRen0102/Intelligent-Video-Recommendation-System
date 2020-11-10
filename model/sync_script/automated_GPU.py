676c5c6dc912d6c634f92451a54da3e6d8433767
Fuyuan Lyu <lfy_1997@126.com>
2020-11-09 20:02:48 -0500

    Sync model between GPU and VM

---
+++ model/sync_script/automated_GPU.py
@@ -0,0 +1,34 @@
+# # 848084185210-q6g400ans0ddc451tlak117dr7icvt8l.apps.googleusercontent.com
+# # E2k0Khce2aMFmo7144jZ7gES
+
+### Calling this function means the developer has decided
+### to use a new model instead of the old one
+
+import os
+import shutil
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+
+gauth = GoogleAuth()
+gauth.LocalWebserverAuth()
+
+drive = GoogleDrive(gauth)
+
+## New model is trained automatically
+
+## Delete last version
+file_list = drive.ListFile({'q': "'root' in parents and trashed=false"}).GetList()
+for file1 in file_list:
+    print('title: %s, id: %s' % (file1['title'], file1['id']))
+    if file1['title'] == 'model.zip':
+        file1.Trash()
+
+
+## Delete 
+shutil.make_archive("model", 'zip', "../checkpoint/model")
+file = drive.CreateFile()
+file.SetContentFile("model.zip")
+file.Upload()
+print(file['id'])
+
+
