7e1fa2802f8a86c3217bc161f8eea550db843d55
Fuyuan Lyu Tommy <lfy_1997@126.com>
2020-11-11 13:14:55 -0500

    Revert "Action"

--- model/sync_script/automated_GPU.py
+++ model/sync_script/automated_GPU.py
@@ -8,26 +8,9 @@ import os
 import shutil
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
-import sys
 
-my_path = os.path.abspath(os.path.dirname(__file__))
-
-# gauth = GoogleAuth()
-# gauth.LoadCredentialsFile("mycreds.txt")
-gauth = GoogleAuth(settings_file = os.path.join(my_path, "settings.yaml"))
-gauth.LoadCredentialsFile(os.path.join(my_path, "mycreds.txt"))
-if gauth.credentials is None:
-    # Authenticate if they're not there
-    gauth.LocalWebserverAuth()
-elif gauth.access_token_expired:
-    # Refresh them if expired
-    gauth.Refresh()
-else:
-    # Initialize the saved creds
-    gauth.Authorize()
-# Save the current credentials to a file
-gauth.SaveCredentialsFile(os.path.join(my_path, "mycreds.txt"))
-
+gauth = GoogleAuth()
+gauth.LocalWebserverAuth()
 
 drive = GoogleDrive(gauth)
 
@@ -39,31 +22,13 @@ for file1 in file_list:
     print('title: %s, id: %s' % (file1['title'], file1['id']))
     if file1['title'] == 'model.zip':
         file1.Trash()
-    elif file1['title'] == 'movies.csv' or file1['title'] == 'explicit_fb.csv':
-    	file1.Trash()
 
 
-## Upload dataset
-file = drive.CreateFile(metadata = {"title": "movies.csv"})
-file.SetContentFile(os.path.join(my_path, '../dataset/final_csv/movies.csv'))
+## Delete 
+shutil.make_archive("model", 'zip', "../checkpoint/model")
+file = drive.CreateFile()
+file.SetContentFile("model.zip")
 file.Upload()
-print("movies.csv", file['id'])
-
-file = drive.CreateFile(metadata = {"title": "explicit_fb.csv"})
-file.SetContentFile(os.path.join(my_path, '../dataset/final_csv/explicit_fb.csv'))
-file.Upload()
-print("explicit_fb.csv", file['id'])
-
-## Upload model
-shutil.make_archive(os.path.join(my_path, "../checkpoint/model"), 'zip', os.path.join(my_path, "../checkpoint/model"))
-file = drive.CreateFile(metadata = {"title": "model.zip"})
-file.SetContentFile(os.path.join(my_path, "../checkpoint/model.zip"))
-file.Upload()
-print("model.zip", file['id'])
-
-
-
-# os.remove(os.path.join(my_path, "../checkpoint/model.zip"))
-
+print(file['id'])
 
 
