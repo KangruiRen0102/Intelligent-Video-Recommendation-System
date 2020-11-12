84f85facacb4b94a2f1d8c2f91fbe67620ebe184
Fuyuan Lyu <lfy_1997@126.com>
2020-11-11 19:40:05 -0500

    add update model

--- model/train/update_model.py
+++ model/train/update_model.py
@@ -5,6 +5,7 @@ if __name__ == '__main__':
 	my_path = os.path.abspath(os.path.dirname(__file__))
 	src = os.path.join(my_path, '../checkpoint/model')
 	dst = os.path.join(my_path, '../checkpoint/baseline_model')
+	print
 	if os.path.isdir(dst):
 		shutil.rmtree(dst)
 	shutil.copytree(src, dst)
