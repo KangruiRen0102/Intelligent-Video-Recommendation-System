27aa34f50b52eb3544285c8895f14ff3164fc829
edrickdch <edrick.dacorteh@gmail.com>
2020-12-03 17:55:40 -0500

    polish startup script

--- startup.sh
+++ startup.sh
@@ -1,4 +1,14 @@
 ### This file should be run from the root of this code repository ###
+
+
+# Creating deployment log file
+dir=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
+FILE=${dir}/deployments/deployments.csv
+if [ ! -f $FILE ]
+then
+    echo "date,commit,version,rollback" >> $FILE
+fi
+
+# Starting stable components of our infrastructure
+docker-compose down
 docker-compose up -d
-echo "date,commit,version,rollback\n" > deployments/deployments.csv
-#bash deployments/cron-daily-deploy.sh
